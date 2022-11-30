dc86d4fddfa9b09199b06df0a5600e4206e03d20
mike-ebrahimi <104879016+mike-ebrahimi@users.noreply.github.com>
2022-11-30 10:43:52 -0700

    updated a miss-naming for login

--- server2/assignment6/assignment6/src/main/java/course/ensf607/assignment6/customer/CustomerController.java
+++ server2/assignment6/assignment6/src/main/java/course/ensf607/assignment6/customer/CustomerController.java
@@ -61,7 +61,7 @@ public class CustomerController {
     
     
     @GetMapping(path = "/login/")
-    public Boolean getProductById(@RequestParam String email, @RequestParam String password ) {
+    public Boolean custmerLogin(@RequestParam String email, @RequestParam String password ) {
 		return customerService.checkCustomerLogin(email, password);
     }
     
