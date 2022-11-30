d41e0360130a45af7218c453124416a781271b60
mike-ebrahimi <104879016+mike-ebrahimi@users.noreply.github.com>
2022-11-30 15:04:02 -0700

    Delete server directory

--- server/testerDatabase.sql
+++
@@ -1,3 +0,0 @@
-create database ensf607; -- Creates the new database
-create user 'springuser'@'%' identified by 'ThePassword'; -- Creates the user
-grant all on ensf607.* to 'springuser'@'%'; -- Gives all privileges to the new user on the newly created database
